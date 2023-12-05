960eada52776761f5d9f5765a4b9a36a8274520f
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2023-12-06 01:16:54 +0530

    spacing in tailwind, boxmodel margin padding etc

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="hidden text-sm font-semibold">Syed</div>;
+}
+
+export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom';
 import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header className="bg-yellow-500">
-      <Link to={'/'}>Fast React Pizza Co.</Link>
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase">
+      <Link className="tracking-widest" to={'/'}>
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
-      <p>Syed</p>
+      <Username />
     </header>
   );
 }
