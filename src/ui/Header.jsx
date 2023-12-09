5d45427a35110ccba2fc9978dcfb6cdda29ce7e7
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2023-12-10 02:48:54 +0530

    configuring tailwind itself

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
+    <header className="font-pizza flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <Link className="tracking-widest" to={'/'}>
         Fast React Pizza Co.
       </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pizza: '#493',
+      },
+    },
+    fontFamily: {
+      sans: 'Roboto Mono, monospace',
+    },
+    height: {
+      screen: '100dvh',
+    },
   },
   plugins: [],
 };
