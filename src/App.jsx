94c8647c3f995a00ab89e742cd67f86ab5e0a33a
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2024-01-06 18:34:59 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       {
-        path: '/',
+        path: '/fast-react-pizza',
         element: <Home />,
       },
       {
