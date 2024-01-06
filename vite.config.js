80a372064fe5f17d1f102ed1df67c3c4defbc86a
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2024-01-06 17:33:59 +0530

    cahgn

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/fast-react-pizza',
+  base: '/fast-react-pizza',
   plugins: [react()],
 });
