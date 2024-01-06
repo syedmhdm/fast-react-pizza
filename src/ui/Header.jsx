cb627e2e8ee6856799865715a5ba456ecf989ae9
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2024-01-06 22:47:48 +0530

    spelling correction

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -6,7 +6,7 @@ function header() {
   return (
     <header className="font-pizza flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <Link className="tracking-widest" to={'/fast-react-pizza'}>
-        test Fast React Pizza Co.
+        Fast React Pizza Co.
       </Link>
       <SearchOrder />
       <Username />
