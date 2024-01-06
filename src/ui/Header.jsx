78ddfc49559d55f784d1fddb71e55909296ae3ce
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2024-01-06 17:30:52 +0530

    githubpage changes

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom';
 import SearchOrder from '../features/order/SearchOrder';
 import Username from '../features/user/Username';
 
-function Header() {
+function header() {
   return (
     <header className="font-pizza flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <Link className="tracking-widest" to={'/'}>
-        Fast React Pizza Co.
+        test Fast React Pizza Co.
       </Link>
       <SearchOrder />
       <Username />
@@ -14,4 +14,4 @@ function Header() {
   );
 }
 
-export default Header;
+export default header;
